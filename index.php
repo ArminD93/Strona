d995314e5549a1cf777c74dc630ce6dbe30bc73c
ArminD93 <armin.derencz@gmail.com>
2017-12-11 14:44:13 +0100

    Bootstrap menu test

--- index.php
+++ index.php
@@ -16,6 +16,10 @@
 		<link rel="shortcut icon" href="favicon.ico" />
 		<!--ikonka strony-->
 
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 		<link rel="stylesheet" href="style.css" type="text/css" />
 		<link rel="stylesheet" href="css/fontello.css" type="text/css" />
 		<link rel="stylesheet" href="css/flaticon.css" type="text/css" />
@@ -43,11 +47,56 @@
 
 			<div class="menu">
 					
-					<a href="menu.php" class="tilelinkhtml5">
-						
-						MENU
-
-					</a>
+			<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+			  <div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				  <span class="baricon-"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>                        
+				</button>
+				<a class="navbar-brand" href="#">Żeglarstwo</a>
+			  </div>
+			  <div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+				  <li class="active"><a href="#">Home</a></li>
+				  <li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+					 <?php 
+							$polaczenie = @mysqli_connect('localhost', 'root', '', 'pai_derencz');
+							if (!$polaczenie) {
+							  die('Wystąpił błąd połączenia: ' . mysqli_connect_errno());
+							}
+							@mysqli_query($polaczenie, 'SET NAMES utf8');
+							
+							$sql = 'SELECT `id`, `nazwa` 
+										FROM `kategorie` 
+										ORDER BY `nazwa`';
+							$wynik = mysqli_query($polaczenie, $sql);
+							if (mysqli_num_rows($wynik) > 0) {
+							  echo "<ul>" . PHP_EOL;
+							  while (($kategoria = @mysqli_fetch_array($wynik))) {
+								echo '<li><a href="' . $_SERVER["PHP_SELF"] . '?kat_id=' . $kategoria['id'] . '">' . $kategoria['nazwa'] . '</a></li>' . PHP_EOL;
+							  }
+							  echo "</ul>" . PHP_EOL;
+							} else {
+							  echo 'wyników 0';
+							}
+							
+					  ?>
+					</ul>
+				  </li>
+				  <li><a href="#">O mnie</a></li>
+				  <li><a href="#">Kontakt</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+				  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			  </div>
+			</div>
+		  </nav>
 			</div>
 
 
